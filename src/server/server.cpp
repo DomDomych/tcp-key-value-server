654921a0d44aee4bd892349afdb3c95cf049dc3e
Damir <damirmatematic@gmail.com>
2026-09-10 09:38:45 +0300

    Add storage mutex to constructor

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,6 +1,8 @@
 #include "server/server.hpp"
 #include "session/session.hpp"
 #include <boost/asio.hpp>
+#include <thread>
+
 
 using tcp = boost::asio::ip::tcp;
 
@@ -12,8 +14,16 @@ Server::Server(boost::asio::io_context &io, unsigned short port)
 void Server::accept_client()
 {
     tcp::socket socket = acceptor_.accept();
-    Session session(std::move(socket), storage_);
-    session.start();
+    
+    std::thread client_thread(
+        [this,socket=std::move(socket)]() mutable
+        {
+            Session session(std::move(socket),storage_);
+            session.start();
+        }
+    );
+
+    client_thread.detach();
 }
 
 void Server::start()
