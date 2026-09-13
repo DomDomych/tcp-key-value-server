ccbce96479f2ca59ea94b45898448b63df34e732
Damir <damirmatematic@gmail.com>
2026-09-13 18:03:47 +0300

    Added basic logics

---
+++ src/storage/storage.cpp
@@ -0,0 +1,38 @@
+#include "storage/storage.hpp"
+
+
+void Storage::set(std::string_view key,std::string_view value)
+{
+    std::unique_lock lock(mutex_);
+    storage_[std::string(key)]=std::string(value);
+
+    return;
+}
+
+std::optional<std::string> Storage::get(std::string_view key) const
+{
+    std::shared_lock lock(mutex_);
+    auto it = storage_.find(std::string(key));
+
+    if(it==storage_.end())
+    {
+        return std::nullopt;
+    }
+
+    return it->second;
+}
+
+bool Storage::del(std::string_view key)
+{
+    std::unique_lock(mutex_);
+
+    auto it = storage_.find(std::string(key));
+
+    if(it==storage_.end())
+    {
+        return false;
+    }
+
+    storage_.erase(std::string(key));
+    return true;
+}
