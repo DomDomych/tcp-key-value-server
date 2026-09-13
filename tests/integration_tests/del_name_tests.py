34beb426d6b74abb5980ae28a67c7f51faab4f92
Damir <damirmatematic@gmail.com>
2026-09-13 19:14:21 +0300

    Changed returning answers of server

--- tests/integration_tests/del_name_tests.py
+++ tests/integration_tests/del_name_tests.py
@@ -6,7 +6,7 @@ sock.connect(("127.0.0.1",8080))
 sock.sendall((b"SET name Damir\n"))
 response=sock.recv(1024)
 
-assert response==b"OK!\n"
+assert response==b"OK\n"
 
 sock.sendall((b"GET name\n"))
 response=sock.recv(1024)
@@ -16,12 +16,12 @@ assert response==b"Damir\n"
 sock.sendall((b"DEL name\n"))
 response=sock.recv(1024)
 
-assert response==b"OK!\n"
+assert response==b"OK\n"
 
 sock.sendall((b"GET name\n"))
 response=sock.recv(1024)
 
-assert response==b"No Such Key!\n"
+assert response==b"NO SUCH KEY\n"
 
 sock.close()
 
--- tests/integration_tests/set_name_test.py
+++ tests/integration_tests/set_name_test.py
@@ -6,7 +6,7 @@ sock.connect(("127.0.0.1",8080))
 sock.sendall(b"SET name Damir\n")
 response=sock.recv(1024)
 
-assert response==b"OK!\n"
+assert response==b"OK\n"
 
 sock.close()
 
