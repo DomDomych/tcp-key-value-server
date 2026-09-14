d483b39b3fb62fa5871fb686a9ebc313c5a0fe9d
Damir <damirmatematic@gmail.com>
2026-09-14 17:34:35 +0300

    Add multiple worker threads

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include "server/server.hpp"
+
 #include <boost/asio.hpp>
+
+#include <algorithm>
+#include <iostream>
+#include <thread>
+#include <vector>
 
 int main()
 {
     boost::asio::io_context io;
 
     Server server(io, 8080);
-
     server.start();
-    io.run();
+
+    const std::size_t thread_count =
+        std::max(1u, std::thread::hardware_concurrency());
+
+    std::vector<std::thread> workers;
+    workers.reserve(thread_count);
+
+    for (std::size_t i = 0; i < thread_count; ++i)
+    {
+        workers.emplace_back([&io]()
+        {
+            io.run();
+        });
+    }
+
+    for (auto& worker : workers)
+    {
+        worker.join();
+    }
 
     return 0;
 }
