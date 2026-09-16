dd2135effec798f8371bd42d2a6b0fd7efef3ed1
Damir <damirmatematic@gmail.com>
2026-09-16 21:14:17 +0300

    Internal structure of LRU_cache

--- include/storage/storage.hpp
+++ include/storage/storage.hpp
@@ -22,21 +22,6 @@ class PostgresStorage
     std::mutex mutex_;
 };
 
-class Storage
-{
-  public:
-    Storage(std::string connection_string);
-
-    std::optional<std::string> get(std::string_view key);
-    bool set(std::string_view key, std::string_view value);
-    bool del(std::string_view key);
-
-  private:
-    PostgresStorage database_;
-    //LruCache cache_;
-};
-
-
 class LruCache
 {
 
@@ -50,5 +35,28 @@ class LruCache
     void erase(std::string_view key);
 
   private:
-  // other incapsulated stuff
+    std::size_t capacity_;
+
+    std::list<std::pair<std::string,std::string>> items_;
+
+    std::unordered_map<
+      std::string,
+      std::list<std::pair<std::string,std::string>>::iterator
+    > index_;
+
+    std::mutex mutex_;
+};
+
+class Storage
+{
+  public:
+    Storage(std::string connection_string);
+
+    std::optional<std::string> get(std::string_view key);
+    bool set(std::string_view key, std::string_view value);
+    bool del(std::string_view key);
+
+  private:
+    PostgresStorage database_;
+    //LruCache cache_;
 };
