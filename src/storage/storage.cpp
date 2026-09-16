73323092705a3b1c7f3235234863273c32e33836
Damir <damirmatematic@gmail.com>
2026-09-16 09:15:16 +0300

    New logic for storage

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -1,29 +1,23 @@
 #include "storage/storage.hpp"
 
-void Storage::set(std::string_view key, std::string_view value)
-{
-    std::unique_lock lock(mutex_);
-    storage_[std::string(key)] = std::string(value);
+#include <utility>
 
-    return;
+Storage::Storage(std::string connection_string)
+    : database_(std::move(connection_string))
+{
 }
 
-std::optional<std::string> Storage::get(std::string_view key) const
+std::optional<std::string> Storage::get(std::string_view key)
 {
-    std::shared_lock lock(mutex_);
-    auto it = storage_.find(std::string(key));
+    return database_.get(key);
+}
 
-    if (it == storage_.end())
-    {
-        return std::nullopt;
-    }
-
-    return it->second;
+bool Storage::set(std::string_view key, std::string_view value)
+{
+    return database_.set(key, value);
 }
 
 bool Storage::del(std::string_view key)
 {
-    std::unique_lock lock(mutex_);
-
-    return storage_.erase(std::string(key)) != 0;
+    return database_.del(key);
 }
