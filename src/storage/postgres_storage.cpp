c9cb7d970d4a71b5d1d45f0eb54823c584b3fd47
Damir <damirmatematic@gmail.com>
2026-09-20 17:17:26 +0300

    Changed exec_params to exec

--- src/storage/postgres_storage.cpp
+++ src/storage/postgres_storage.cpp
@@ -19,7 +19,7 @@ std::optional<std::string> PostgresStorage::get(std::string_view key)
     pqxx::read_transaction transaction{connection_};
 
     auto result =
-        transaction.exec_params("SELECT value FROM kv_store WHERE key = $1", pqxx::params{key});
+        transaction.exec("SELECT value FROM kv_store WHERE key = $1", pqxx::params{key});
 
     if (result.empty())
     {
@@ -37,7 +37,7 @@ bool PostgresStorage::set(std::string_view key, std::string_view value)
     {
         pqxx::work transaction{connection_};
 
-        transaction.exec_params("INSERT INTO kv_store (key, value)"
+        transaction.exec("INSERT INTO kv_store (key, value)"
                                 "VALUES ($1, $2) "
                                 "ON CONFLICT (key) "
                                 "DO UPDATE SET value = EXCLUDED.value",
@@ -62,7 +62,7 @@ bool PostgresStorage::del(std::string_view key)
         pqxx::work transaction{connection_};
 
         auto result =
-            transaction.exec_params("DELETE FROM kv_store WHERE key = $1", pqxx::params{key});
+            transaction.exec("DELETE FROM kv_store WHERE key = $1", pqxx::params{key});
 
         const bool deleted = result.affected_rows() != 0;
 
